63841c2d62b838a14ac8b975fef4a44953877102
Tharsha Loganathan <tharshzlog@gmail.com>
2020-09-15 17:45:28 +0530

    Updates

---
+++ precache-manifest.4fc01b454a62cfd8bf5248864a002ac6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161aec9bfcb8a4a35d79e6d991f2f9bb",
+    "url": "/Portfolio_ReactApp/index.html"
+  },
+  {
+    "revision": "5e732f657d1722232b0f",
+    "url": "/Portfolio_ReactApp/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "24cddae2ed7bbd9f188a",
+    "url": "/Portfolio_ReactApp/static/css/main.0da89eab.chunk.css"
+  },
+  {
+    "revision": "5e732f657d1722232b0f",
+    "url": "/Portfolio_ReactApp/static/js/2.7584bf7d.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Portfolio_ReactApp/static/js/2.7584bf7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24cddae2ed7bbd9f188a",
+    "url": "/Portfolio_ReactApp/static/js/main.b0c9686d.chunk.js"
+  },
+  {
+    "revision": "93a715165f0749779fdb",
+    "url": "/Portfolio_ReactApp/static/js/runtime-main.75e4d9cf.js"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/Portfolio_ReactApp/static/media/img1.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/Portfolio_ReactApp/static/media/img2.90616a5d.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/Portfolio_ReactApp/static/media/img3.54fd4234.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Portfolio_ReactApp/precache-manifest.4fc01b454a62cfd8bf5248864a002ac6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio_ReactApp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
